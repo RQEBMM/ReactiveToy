e861ea8fab3d378ffccf52ecd52ecc41628a067d
akhalsa <akhalsa43@gmail.com>
2016-08-26 21:29:22 -0500

    add car

--- ReactiveToy/ReactiveToy/Car.swift
+++ ReactiveToy/ReactiveToy/Car.swift
@@ -8,5 +8,19 @@
 
 import Foundation
 class Car{
+    var vin:String
+    var exteriorColor:String
+    var interiorColor:String
+    var trimId:String
+    var dealershipId:String
+    var trim:Trim?
+    var dealership:Dealership?
     
+    init(v:String, e:String, i:String, tid:String, did:String){
+        vin = v
+        exteriorColor = e
+        interiorColor = i
+        trimId = tid
+        dealershipId = did
+    }
 }
