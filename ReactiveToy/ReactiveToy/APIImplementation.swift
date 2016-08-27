f42b46b5a19539f43e0893a447226dfd13317af8
akhalsa <akhalsa43@gmail.com>
2016-08-26 21:01:06 -0500

    ok this is the contract

---
+++ ReactiveToy/ReactiveToy/APIImplementation.swift
@@ -0,0 +1,30 @@
+//
+//  APIImplementation.swift
+//  ReactiveToy
+//
+//  Created by Avtar Khalsa on 8/26/16.
+//  Copyright © 2016 AB. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+class APIImplementation{
+    
+}
+extension APIImplementation : APIWrapper{
+    func beginMonitoringCars() -> Observable<Car>{
+        return Observable.create({ (subcriber) -> Disposable in
+            //ill do stuff here to hit the api
+        })
+    }
+    func fetchDealershipWithId(id:String)->Observable<Dealership>{
+        return Observable.create({ (subcriber) -> Disposable in
+            
+        })
+    }
+    func fetchTrimWithTrimId(id:String)->Observable<Trim>{
+        return Observable.create({ (subscriber) -> Disposable in
+            
+        })
+    }
+}
---
+++ ReactiveToy/ReactiveToy/ServiceLocator.swift
@@ -0,0 +1,15 @@
+//
+//  ServiceLocator.swift
+//  ReactiveToy
+//
+//  Created by Avtar Khalsa on 8/26/16.
+//  Copyright © 2016 AB. All rights reserved.
+//
+
+import Foundation
+
+class ServiceLocator{
+    static let sharedInstance = ServiceLocator()
+    
+    lazy mWrapper:APIWrapper = 
+}
