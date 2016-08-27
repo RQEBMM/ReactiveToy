796d226e94aaa7113fc437e6958086f51aa93d02
akhalsa <akhalsa43@gmail.com>
2016-08-27 16:16:51 -0500

    ids on cars

--- ReactiveToy/ReactiveToy/Car.swift
+++ ReactiveToy/ReactiveToy/Car.swift
@@ -16,22 +16,26 @@ class Car {
     var model:String?
     var trimId:String?
     var dealershipId:String?
+    var id:String
     
     var trim:Trim?
     var dealership:Dealership?
     
-    init(v:String, m:String, tid:String, did:String) {
+    init(v:String, m:String, tid:String, did:String, server_id:String) {
         vin = v
         model = m
         trimId = tid
         dealershipId = did
+        self.id = server_id
     }
     
-    required init?(json: JSON) {
+    required init?(json: JSON, server_id:String) {
         self.vin = "vin" <~~ json
         self.model = "model" <~~ json
         self.trimId = "trim_id" <~~ json
         self.dealershipId = "dealership_id" <~~ json
+        self.id = server_id
+        
     }
     
 }
