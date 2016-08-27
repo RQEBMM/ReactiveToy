d3108ad622fd82a221d09d5a783933ce4fb1e88f
akhalsa <akhalsa43@gmail.com>
2016-08-26 21:41:30 -0500

    initialize the observables

--- ReactiveToy/ReactiveToy/APIImplementation.swift
+++ ReactiveToy/ReactiveToy/APIImplementation.swift
@@ -16,12 +16,12 @@ class APIImplementation{
 }
 extension APIImplementation : APIWrapper{
     func beginMonitoringCars() -> Observable<[Car]>{
-        return Observable.just([Car.init()])
+        return Observable.just([Car.init(v: "231342423423423", e: "White", i: "Tan", tid: "123", did: "555")])
     }
     func fetchDealershipWithId(id:String)->Observable<Dealership>{
-        return Observable.just(Dealership.init())
+        return Observable.just(Dealership.init(n: "Tom's Ford Dealership", a: "123 Main St", lat: 30.2324, lng: -97.321))
     }
     func fetchTrimWithTrimId(id:String)->Observable<Trim>{
-        return Observable.just(Trim.init())
+        return Observable.just(Trim.init(m: 3250, e: "4Liter Turbo", mpg: 25.52, n: "Premium"))
     }
 }
